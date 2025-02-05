072949a4ea2a402a3a5938a5e89d2a7f9051bf2a
Aline Soares <linesoares.s@hotmail.com>
2025-02-05 08:24:45 -0300

    Compra Ingresso

---
+++ js/app.js
@@ -0,0 +1,40 @@
+let qtdePista = document.getElementById('qtd-pista');
+let qtdeSuperior = document.getElementById('qtd-superior');
+let qtdeInferior = document.getElementById('qtd-inferior');
+
+function comprar(){
+    let qtdeSelecionada =document.getElementById('qtd').value;
+    let ingressoSelecionado = document.getElementById('tipo-ingresso').value;
+    if (ingressoSelecionado == 'inferior'){
+        if(qtdeSelecionada > qtdeInferior.textContent){
+            alert(`Quantidade indisponível, você pode selecionar no máximo ${qtdeInferior.textContent}`);
+        }else{
+            let qtdeAtualizada= qtdeInferior.textContent-qtdeSelecionada;
+            qtdeInferior.textContent = qtdeAtualizada;
+            document.getElementById('qtd').value = 0;
+             alert('Compra Realizada com Sucesso');
+        };
+
+    }else{
+        if(ingressoSelecionado == 'superior'){
+            if(qtdeSelecionada > qtdeSuperior.textContent){
+                alert(`Quantidade indisponível, você pode selecionar no máximo ${qtdeSuperior.textContent}`);
+            }else{
+                let qtdeAtualizada= qtdeSuperior.textContent-qtdeSelecionada;
+                qtdeSuperior.textContent = qtdeAtualizada;
+                document.getElementById('qtd').value = 0;
+                alert('Compra Realizada com Sucesso');
+            };
+        
+        }else{
+            if(qtdeSelecionada > qtdePista.textContent){
+                alert(`Quantidade indisponível, você pode selecionar no máximo ${qtdePista.textContent}`);
+            }else{
+                let qtdeAtualizada= qtdePista.textContent-qtdeSelecionada;
+                qtdePista.textContent = qtdeAtualizada;
+                document.getElementById('qtd').value = 0;
+                alert('Compra Realizada com Sucesso');
+            };
+        };
+    };
+};
